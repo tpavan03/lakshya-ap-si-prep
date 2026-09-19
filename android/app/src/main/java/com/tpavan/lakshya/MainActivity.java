823c712593822f15a229163824d8c046dbf18584
tpavan03 <thokalapavan.pp@gmail.com>
2026-09-19 09:41:18 +0530

    feat(android): add installable Capacitor application

---
+++ android/app/src/main/java/com/tpavan/lakshya/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tpavan.lakshya;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.tpavan.lakshya',
+  appName: 'Lakshya AP SI Prep',
+  webDir: 'dist',
+  android: {
+    allowMixedContent: false,
+    backgroundColor: '#142b38',
+  },
+}
+
+export default config
